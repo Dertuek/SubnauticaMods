b889c6fe2e50fa4f89cdb53ca1077849aef50ce8
Bibifoc95 <onlinetuek@free.fr>
2022-08-13 16:38:47 +0200

    Add SignColorFix Mod

---
+++ SignColorFix/QMod.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+
+namespace SignColorFix
+{
+    public class QMod
+    {
+        public static void Patch()
+        {
+            try
+            {
+                var harmony = new Harmony("com.signcolorfix.mod");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"SignColorFix patch failed!\n{FormatException(e)}");
+            }
+        }
+        private static string FormatException(Exception e)
+        {
+            if (e == null)
+                return string.Empty;
+            return $"\"Exception: {e.GetType()}\"\n\tMessage: {e.Message}\n\tStacktrace: {e.StackTrace}\n" +
+                   FormatException(e.InnerException);
+        }
+    }
+}
---
+++ SignColorFix/uGUI_SignInput_UpdateScale_Patch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+
+
+namespace SignColorFix
+{
+
+	[HarmonyPatch(typeof(uGUI_SignInput), "UpdateScale")]
+	class uGUI_SignInput_UpdateScale_Patch
+	{
+		[HarmonyPrefix]
+		static bool Prefix(uGUI_SignInput __instance)
+		{
+
+			if (! __instance.gameObject.GetComponentInParent<Constructable>())
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+	}
+
+}
